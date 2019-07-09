cdde4d55ba1fb015cb75059c84c8bd79cb37bfa1
Zzz9898 <1617386850@qq.com>
2019-07-09 09:23:55 +0800

    First commit all!

---
+++ src/main/java/com/zjw/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.zjw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello boot";
+    }
+}
